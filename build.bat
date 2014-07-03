8bd39b614fb001ae03c7ce7d175779da6f909f62
Yusuke Shinyama <yusuke@shinyama.jp>
2014-07-03 18:00:19 +0900

    Build script changed.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @set FLEX_HOME=%UserProfile%\flex_sdk_4.6
-java -jar %FLEX_HOME%\lib\mxmlc.jar +flexlib=%FLEX_HOME%\frameworks -static-rsls -compiler.source-path=./src -o .\bin\vgaplayer.swf .\src\Main.as
+java -jar %FLEX_HOME%\lib\mxmlc.jar +flexlib=%FLEX_HOME%\frameworks -static-rsls -o .\bin\vgaplayer.swf -compiler.source-path=./src .\src\Main.as
 @if errorlevel 1 (
  pause
  exit /b 
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ JAVA=java
 FLEX_HOME=${FLEX_HOME:-$HOME/flex_sdk_4.6}
 MXMLC="$JAVA -jar $FLEX_HOME/lib/mxmlc.jar +flexlib=$FLEX_HOME/frameworks"
 
-$MXMLC -static-rsls -compiler.source-path=./src/ -o ./bin/vgaplayer.swf ./src/Main.as
+$MXMLC -static-rsls -o ./bin/vgaplayer.swf -compiler.source-path=./src/ ./src/Main.as
